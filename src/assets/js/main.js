f01722a54f52dc24c1ef06f21fc1572786e5e424
daonghia2404 <daonghia2404@gmail.com>
2021-05-10 23:09:51 +0700

    UI

---
+++ src/assets/js/main.js
@@ -0,0 +1,79 @@
+window.onload = () => {
+  // tabEvent.init();
+  // owlCarousel.init();
+  header.init()
+}
+
+const header = {
+  init: function() {
+    this.expandMobileMenu()
+  },
+  expandMobileMenu: function() {
+    const btnMenu = document.querySelector('.header .btn-menu-mobile')
+    const mainMenu = document.querySelector('.header .header-lists')
+    if (btnMenu && mainMenu) {
+      btnMenu.addEventListener('click', () => {
+        mainMenu.classList.toggle('active')
+      })
+    }
+  },
+}
+
+const owlCarousel = {
+  init: function () {
+    this.setupYourCarousel();
+  },
+  setupYourCarousel: function () {
+    var $owl = $("#banner-carousel").owlCarousel({
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 1,
+        },
+      },
+      loop: true,
+      rewind: false,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      smartSpeed: 600,
+      mouseDrag: true,
+      nav: true,
+      dots: true,
+      navText: [
+        "<img src='./assets/icons/icon-angle-left-blue.svg'>",
+        "<img src='./assets/icons/icon-angle-right-blue.svg'>",
+      ],
+    });
+    $owl.trigger("refresh.owl.carousel");
+  },
+};
+
+const tabEvent = {
+  init: function () {
+    this.setupTabEvent();
+  },
+  setupTabEvent: function () {
+    const main = document.querySelectorAll(".tab-wrapper");
+    if (main.length !== 0) {
+      main.forEach((mainTarget) => {
+        const tabClick = mainTarget.querySelectorAll(".tabs-group .tab-item");
+        const tabMain = mainTarget.querySelectorAll(
+          ".tabs-main-group .tab-item"
+        );
+
+        tabClick.forEach((item, index) =>
+          item.addEventListener("click", () => {
+            tabClick.forEach((i) => i.classList.remove("active"));
+            tabMain.forEach((i) => i.classList.remove("active"));
+
+            tabClick[index].classList.add("active");
+            tabMain[index].classList.add("active");
+          })
+        );
+      });
+    }
+  },
+};
